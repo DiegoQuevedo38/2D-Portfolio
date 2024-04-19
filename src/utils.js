b7f206ff9215d543c25ddd78a896273f1e5ccc0b
DiegoQuevedo38 <quevedodiego3008@gmail.com>
2024-04-19 14:55:16 +0200

    dialogue ui autocompleting text

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,48 @@
 // Functions 
-
 export function displayDialogue(text, onDisplayEnd) {
+    const dialogueUI = document.getElementById("textbox-container");
+    const dialogue = document.getElementById("dialogue");
+
+    dialogueUI.style.display = "block";
+
+    let index = 0;
+    let currentText = "";
+    const intervalRef = setInterval(() => {
+        if(index < text.length) {
+            currentText += text[index];
+            dialogue.innerHTML = currentText;
+            index++;
+            return;
+        }
+
+        clearInterval(intervalRef);
+    }, 5);
+
+    const closeBtn = document.getElementById("close");
+
+    function onCloseBtn() {
+        onDisplayEnd();
+        dialogueUI.style.display = "none";
+        dialogue.innerHTML = "";
+        clearInterval(intervalRef);
+        closeBtn.removeEventListener("click", onCloseBtn);
+    }
+
+    closeBtn.addEventListener("click", onCloseBtn);
+
+    // Agregar la funcionalidad para avanzar al siguiente diálogo al hacer clic
+    dialogueUI.addEventListener("click", () => {
+        if (index < text.length) {
+            clearInterval(intervalRef);
+            index = text.length;
+            dialogue.innerHTML = text;
+        } else {
+            clearInterval(intervalRef);
+        }
+    });
+}
+
+/* export function displayDialogue(text, onDisplayEnd) {
     const dialogueUI = document.getElementById("textbox-container");
     const dialogue = document.getElementById("dialogue");
 
@@ -30,7 +72,7 @@ export function displayDialogue(text, onDisplayEnd) {
     }
 
     closeBtn.addEventListener("click", onCloseBtn)
-}
+} */
 
 export function setCamScale(k) {
     const resizeFactor = k.width() / k.height();
