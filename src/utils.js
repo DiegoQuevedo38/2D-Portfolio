ab44dd3926591db72af00301817faf95e0063ba0
DiegoQuevedo38 <quevedodiego3008@gmail.com>
2024-04-19 15:02:17 +0200

    dialogue fn updated to array control

--- src/utils.js
+++ src/utils.js
@@ -8,14 +8,18 @@ export function displayDialogue(text, onDisplayEnd) {
     let index = 0;
     let currentText = "";
     const intervalRef = setInterval(() => {
-        if(index < text.length) {
-            currentText += text[index];
-            dialogue.innerHTML = currentText;
+        if (index < dialogueArray.length) {
+            const currentPart = dialogueArray[index];
+            if (currentText.length < currentPart.length) {
+                currentText += currentPart[index];
+                dialogue.innerHTML = currentText;
+                return;
+            }
             index++;
-            return;
+            currentText = "";
+        } else {
+            clearInterval(intervalRef);
         }
-
-        clearInterval(intervalRef);
     }, 5);
 
     const closeBtn = document.getElementById("close");
@@ -30,49 +34,12 @@ export function displayDialogue(text, onDisplayEnd) {
 
     closeBtn.addEventListener("click", onCloseBtn);
 
-    // Agregar la funcionalidad para avanzar al siguiente diálogo al hacer clic
     dialogueUI.addEventListener("click", () => {
-        if (index < text.length) {
-            clearInterval(intervalRef);
-            index = text.length;
-            dialogue.innerHTML = text;
-        } else {
-            clearInterval(intervalRef);
-        }
+        clearInterval(intervalRef);
+        index++;
+        currentText = "";
     });
 }
-
-/* export function displayDialogue(text, onDisplayEnd) {
-    const dialogueUI = document.getElementById("textbox-container");
-    const dialogue = document.getElementById("dialogue");
-
-    dialogueUI.style.display = "block";
-
-    let index = 0;
-    let currentText = "";
-    const intervalRef = setInterval(() => {
-        if(index < text.length) {
-            currentText += text[index];
-            dialogue.innerHTML = currentText;
-            index++;
-            return;
-        }
-
-        clearInterval(intervalRef)
-    }, 5)
-
-    const closeBtn = document.getElementById("close");
-
-    function onCloseBtn() {
-        onDisplayEnd();
-        dialogueUI.style.display = "none";
-        dialogue.innerHTML = "";
-        clearInterval(intervalRef);
-        closeBtn.removeEventListener("click", onCloseBtn)
-    }
-
-    closeBtn.addEventListener("click", onCloseBtn)
-} */
 
 export function setCamScale(k) {
     const resizeFactor = k.width() / k.height();
