f24425d37fca4aeefb89b1c0d63a4c6560fb9615
DiegoQuevedo38 <quevedodiego3008@gmail.com>
2024-04-21 01:38:29 +0200

    bugs fixed

--- src/utils.js
+++ src/utils.js
@@ -1,54 +1,4 @@
 // Functions 
-/* export function displayDialogue(text, onDisplayEnd) {
-    const dialogueUI = document.getElementById("textbox-container");
-    const dialogue = document.getElementById("dialogue");
-
-    dialogueUI.style.display = "block";
-
-    let index = 0;
-    let currentText = "";
-    let intervalRef;
-
-    function displayNextLetter() {
-        if (index <= text.length) {
-            const currentPart = text[index];
-            if (currentText.length < currentPart.length) {
-                currentText += currentPart[currentText.length];
-                dialogue.innerHTML = currentText;
-                return;
-            }
-            clearInterval(intervalRef);
-        } else {
-            clearInterval(intervalRef);
-        }
-    }
-
-    intervalRef = setInterval(displayNextLetter, 5);
-
-    const closeBtn = document.getElementById("close");
-
-    function onCloseBtn() {
-        onDisplayEnd();
-        dialogueUI.style.display = "none";
-        dialogue.innerHTML = "";
-        clearInterval(intervalRef);
-        currentText = "";
-        closeBtn.removeEventListener("click", onCloseBtn);
-    }
-
-    closeBtn.addEventListener("click", onCloseBtn);
-
-    dialogueUI.addEventListener("click", () => {
-        if (index < text.length) {
-            clearInterval(intervalRef);
-            index++;
-            currentText = "";
-            intervalRef = setInterval(displayNextLetter, 5);
-        } else {
-            intervalRef = setInterval(displayNextLetter, 5);
-        }
-    });
-} */
 
 export function displayDialogue(text, onDisplayEnd) {
     const dialogueUI = document.getElementById("textbox-container");
@@ -74,9 +24,9 @@ export function displayDialogue(text, onDisplayEnd) {
         }
     }
 
-    intervalRef = setInterval(displayNextLetter, 5);
-
     const closeBtn = document.getElementById("close");
+
+    intervalRef = setInterval(displayNextLetter, 5);
 
     function onCloseBtn() {
         onDisplayEnd();
@@ -92,14 +42,19 @@ export function displayDialogue(text, onDisplayEnd) {
     dialogueUI.addEventListener("click", () => {
         if (currentText !== text[index]) {
             currentText = text[index];
-            dialogue.innerHTML = currentText;
-            if (index == text.length){
+            dialogue.innerHTML = text[index];
+            if (index >= text.length -1){
                 onCloseBtn()
+                return
             } 
-        } 
             clearInterval(intervalRef);
-            index++;
-            currentText = "";
+            return
+        } 
+        clearInterval(intervalRef);
+        index = index + 1;
+        currentText = "";
+        intervalRef = setInterval(displayNextLetter, 5);
+        return
     });
 }
 
